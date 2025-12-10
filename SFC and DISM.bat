32b6fdb80b03443e18482ea8ce6f078af53e5c7b
Shamrock Cossar <89975338+Shammy01011101@users.noreply.github.com>
2025-12-10 12:42:57 +1000

    Add files via upload

---
+++ SFC and DISM.bat
@@ -0,0 +1,10 @@
+Dism /Online /Cleanup-Image /CheckHealth
+Dism /Online /Cleanup-Image /ScanHealth
+Dism /Online /Cleanup-Image /RestoreHealth
+Dism /Online /Cleanup-Image /AnalyzeComponentStore
+Dism /online /Cleanup-Image /StartComponentCleanup
+Dism /online /Cleanup-Image /StartComponentCleanup /ResetBase
+Dism /Online /Cleanup-Image /RestoreHealth
+sfc /scannow
+sfc /scannow
+pause
